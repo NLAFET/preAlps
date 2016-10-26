8f1a772a8420ac9227e2be5222890dcdea055793
Alan Ayala <alan.ayala-obregon@inria.fr>
2016-10-26 18:48:05 +0200

    headers-updated

--- include/spTP_utils.h
+++ include/spTP_utils.h
@@ -3,8 +3,15 @@
 #include <string.h>
 #include <mpi.h>
 #include "SuiteSparseQR_C.h"
-void preAlps_TP_parameters_display( char **matrixName, int *k, int ordering, int *printSVal, int *checkFact,
+void preAlps_TP_parameters_display(MPI_Comm comm, char **matrixName, int *k, int ordering, int *printSVal, int *checkFact,
   int *printFact, int argc, char **argv);
-void preAlps_TP_matrix_distribute(int rank, int size, int *row_indx, int *col_indx, double *a, int m, int n, int nnz,
-  long *col_offset, cholmod_sparse **A, int checkFact, cholmod_sparse **A_test, cholmod_common *cc);
+
+void preAlps_CSC_to_cholmod_l_sparse(int m, int n, int nnz, int *colPtr, int *rowInd, double *a, cholmod_sparse **A, cholmod_common *cc);
+
+void preAlps_l_CSC_to_cholmod_l_sparse(long m, long n, long nnz, long *colPtr, long *rowInd, double *a, cholmod_sparse **A, cholmod_common *cc);
+
+void preAlps_spTP_distribution(MPI_Comm comm, int *m, int *n, int *nnz, int **colPtr, int **rowInd,
+  double **a, long *col_offset, int checkFact);
+
+
 #endif
--- include/tournamentPivoting.h
+++ include/tournamentPivoting.h
@@ -14,7 +14,6 @@
   #include <lapacke.h>
 #endif
 
-#define debug(a_,...) printf((a_),__VA_ARGS__)
 #define ASSERT(t_) if(!(t_)) printf("WARNING %s, line %d\n",__FILE__,__LINE__)
 #ifndef MAX
 #define MAX(a_,b_) ((a_)>(b_)?(a_):(b_))
@@ -22,13 +21,14 @@
 #define DBL_EPSILON 1e-15
 
 
-int preAlps_tournamentPivoting(int *row_indx, int *col_indx, double *a, int m,  int n,  int nnz, int k, long *Jc,
-    double **Sval, int printSVal, int ordering);
+int preAlps_tournamentPivoting(MPI_Comm comm, int *colPtr, int *rowInd, double *a, int m,  int n,  int nnz, long col_offset, int k, long *Jc,
+   double **Sval, int printSVal, int ordering);
 
-int preAlps_tournamentPivotingQR(int *row_indx, int *col_indx, double *a, int m,  int n,  int nnz, int k,
-    long *Jc, double **Sval, int printSVal, int checkFact, int printFact, int ordering);
+int preAlps_tournamentPivotingQR(MPI_Comm comm, int *colPtr, int *rowInd, double *a, int m,  int n,  int nnz,
+  long col_offset, int k, long *Jc, double **Sval, int printSVal, int checkFact, int printFact, int ordering);
 
-int preAlps_tournamentPivotingCUR(int *row_indx, int *col_indx, double *a, int m,  int n,  int nnz, int k, long *Jr,
-    long *Jc, double **Sval, int printSVal, int checkFact, int printFact, int ordering);
+
+int preAlps_tournamentPivotingCUR(MPI_Comm comm, int *colPtr, int *rowInd, double *a, int m,  int n,  int nnz,
+    long col_offset, int k, long *Jr, long *Jc, double **Sval, int printSVal, int checkFact, int printFact, int ordering);
 
 #endif
