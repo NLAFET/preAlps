eef3393cc7bcc73234ff61b5ce5b2fc676489013
Simplice Donfack <simplice.donfack@inria.fr>
2018-05-31 12:03:37 +0200

    version of matMult which takes into account the communicator and not mpi_comm_world.

--- utils/preAlps_cplm_matcsr.h
+++ utils/preAlps_cplm_matcsr.h
@@ -163,4 +163,36 @@ int CPLM_MatCSRSymRACScaling(CPLM_Mat_CSR_t *A, double *R, double *C);
 /* Transpose a matrix */
 int CPLM_MatCSRTranspose(CPLM_Mat_CSR_t *A_in, CPLM_Mat_CSR_t *B_out);
 
+
+
+// Convert colPos and shift colInd in order to extract more easily
+// col panels of A during matmult: Fix of CPLM_MatCSRInitializeMatMult_v2()
+int CPLM_MatCSRInitializeMatMult_v2(CPLM_Mat_CSR_t* A_io,
+                            CPLM_IVector_t* pos_in,
+                            CPLM_IVector_t* colPos_in,
+                            CPLM_IVector_t* rowPtr_out,
+                            CPLM_storage_type_t stor_type,
+                            CPLM_IVector_t* dest,
+                            int nrecv,
+                            MPI_Comm comm);
+
+// Convert back to get initial A_io: fix of  CPLM_MatCSRFinalizeMatMult()
+int CPLM_MatCSRFinalizeMatMult_v2(CPLM_Mat_CSR_t* A_io,
+                          CPLM_IVector_t* pos_in,
+                          CPLM_IVector_t* colPos_in,
+                          CPLM_storage_type_t stor_type,
+                          CPLM_IVector_t* dest,
+                          int nrecv,
+                          MPI_Comm comm);
+
+int CPLM_MatCSRMatMult_v2(CPLM_Mat_CSR_t   *A_in,
+                  CPLM_Mat_Dense_t *RHS_in,
+                  CPLM_IVector_t   *dest,
+                  int          nrecv,
+                  CPLM_Mat_Dense_t *C_out,
+                  CPLM_IVector_t   *pos,
+                  CPLM_IVector_t   *colPos,
+                  CPLM_IVector_t   *rowPtr_io,
+                  MPI_Comm     comm,
+                  int          matMultVersion);
 #endif
